1aec7f2712345080f94873930ba2a006323c3d41
Ilya <iluhamishkin@gmail.com>
2021-02-19 23:32:38 +0100

    Separated /admin with /api. Created new api for footer

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,8 +1,9 @@
-module.exports = ({ env }) => ({
+module.exports = ({env}) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'http://strapi:1337/api',
+  url: 'http://localhost/api',
   admin: {
+    url: 'http://localhost/dashboard',
     auth: {
       secret: env('ADMIN_JWT_SECRET', '122df5c206c9f431698d4c329d1a7cb5'),
     },
