ef9bb5169d93a15352f1e8fcbe909fb28676d8f9
Ilya <iluhamishkin@gmail.com>
2021-02-28 02:23:31 +0100

    Changes

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({env}) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: `http://vps-c8ffb20c.vps.ovh.net/api`,
+  url: `http://localhost/api`,
   admin: {
-    url: 'http://vps-c8ffb20c.vps.ovh.net/dashboard',
+    url: 'http://localhost/dashboard',
     auth: {
       secret: env('ADMIN_JWT_SECRET', '122df5c206c9f431698d4c329d1a7cb5'),
     },
