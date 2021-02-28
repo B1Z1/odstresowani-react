0e732a615b942f4e5ec1174d9851cae023ca0720
Ilya <iluhamishkin@gmail.com>
2021-02-28 02:14:37 +0100

    Changed hosts in applications

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({env}) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'http://localhost/api',
+  url: `http://vps-c8ffb20c.vps.ovh.net/api`,
   admin: {
-    url: 'http://localhost/dashboard',
+    url: 'http://vps-c8ffb20c.vps.ovh.net/dashboard',
     auth: {
       secret: env('ADMIN_JWT_SECRET', '122df5c206c9f431698d4c329d1a7cb5'),
     },
