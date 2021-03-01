0368eb7267ab22e1bb32cdede68ebef535e60618
Ilya <iluhamishkin@gmail.com>
2021-03-01 20:08:11 +0100

    Fixed strapi for production

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({env}) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'https://odstresowani/api',
+  url: 'https://odstresowani.pl/api',
   admin: {
-    url: 'https://dstresowani/dashboard',
+    url: 'https://odstresowani.pl/dashboard',
     auth: {
       secret: env('ADMIN_JWT_SECRET', '122df5c206c9f431698d4c329d1a7cb5'),
     },
