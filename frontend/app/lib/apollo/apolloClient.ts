b991a9a0a0aa8b95590ee41d02a9c49246831f7b
Ilya <iluhamishkin@gmail.com>
2021-03-01 19:25:01 +0100

    Fixes in apollo

--- frontend/app/lib/apollo/apolloClient.ts
+++ frontend/app/lib/apollo/apolloClient.ts
@@ -9,7 +9,7 @@ function createApolloClient(): ApolloClient<NormalizedCacheObject> {
     return new ApolloClient({
         ssrMode: isBrowser,
         link: new HttpLink({
-            uri: !isBrowser ? `${process.env.NEXT_PUBLIC_HOST_API_URL}/graphql` : 'http://nginx/api/graphql',
+            uri: isBrowser ? `${process.env.NEXT_PUBLIC_HOST_API_URL}/graphql` : 'http://nginx/api/graphql',
             credentials: 'same-origin'
         }),
         cache: new InMemoryCache()
