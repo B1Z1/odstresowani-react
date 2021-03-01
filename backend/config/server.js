422dffc7faa8ad519c5950773a75138436c3cb8c
Ilya <iluhamishkin@gmail.com>
2021-03-01 19:38:39 +0100

    Fixed strapi for production

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({env}) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'http://localhost/api',
+  url: 'http://odstresowani/api',
   admin: {
-    url: 'http://localhost/dashboard',
+    url: 'http://dstresowani/dashboard',
     auth: {
       secret: env('ADMIN_JWT_SECRET', '122df5c206c9f431698d4c329d1a7cb5'),
     },
