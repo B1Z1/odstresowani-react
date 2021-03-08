320b8082aa54ca12976dab88916f9e2119bbd26a
Ilya <iluhamishkin@gmail.com>
2021-03-09 00:50:29 +0100

    [FRONTEND] - Change env variables

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,21 +1,27 @@
 require('dotenv').config({path: `/usr/app/environments/.env.${process.env.APP_ENV}`});
 
-module.exports = {
-    webpackDevMiddleware: config => {
-        config.watchOptions = {
-            poll: 1000,
-            aggregateTimeout: 300,
-        }
+module.exports = () => {
+    const isDevelopment = process.env.APP_ENV === 'development';
+    const isProduction = process.env.APP_ENV === 'production';
+    const isStaging = process.env.APP_ENV === 'staging';
 
-        return config
-    },
-    generateBuildId: async () => {
-        return 'frontend'
-    },
-    env: {
-        NEXT_PUBLIC_HOST_URL: process.env.NEXT_PUBLIC_HOST_URL,
-        NEXT_PUBLIC_HOST_API_URL: process.env.NEXT_PUBLIC_HOST_API_URL,
-        HOST_SSR_URL: process.env.HOST_SSR_URL,
-        HOST_SSR_API_URL: process.env.HOST_SSR_API_URL
+    return {
+        webpackDevMiddleware: config => {
+            config.watchOptions = {
+                poll: 1000,
+                aggregateTimeout: 300,
+            }
+
+            return config
+        },
+        generateBuildId: async () => {
+            return 'frontend'
+        },
+        env: {
+            NEXT_PUBLIC_HOST_URL: isProduction ? 'https://odstresowani.pl' : 'http://localhost',
+            NEXT_PUBLIC_HOST_API_URL: isProduction ? 'https://odstresowani.pl/api' : 'http://localhost/api',
+            HOST_SSR_URL: isProduction ? 'https://nginx' : 'http://nginx',
+            HOST_SSR_API_URL: isProduction ? 'https://nginx/api' : 'http://nginx/api'
+        }
     }
 }
