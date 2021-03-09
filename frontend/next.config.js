fbbeb9270a563fc1103cb06a3ca95348fc736caf
Ilya <iluhamishkin@gmail.com>
2021-03-09 01:26:13 +0100

    [FRONTEND] - Change env variables

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,25 +1,21 @@
-module.exports = () => {
-    const isDevelopment = process.env.APP_ENV === 'development';
-    const isProduction = process.env.APP_ENV === 'production';
-    const isStaging = process.env.APP_ENV === 'staging';
+require('dotenv').config({path: `./environments/.env.${process.env.APP_ENV}`});
 
-    return {
-        webpackDevMiddleware: config => {
-            config.watchOptions = {
-                poll: 1000,
-                aggregateTimeout: 300,
-            }
-
-            return config
-        },
-        generateBuildId: async () => {
-            return 'frontend'
-        },
-        env: {
-            NEXT_PUBLIC_HOST_URL: isProduction && !isStaging ? 'https://odstresowani.pl' : 'http://localhost',
-            NEXT_PUBLIC_HOST_API_URL: isProduction && !isStaging ? 'https://odstresowani.pl/api' : 'http://localhost/api',
-            HOST_SSR_URL: 'http://nginx',
-            HOST_SSR_API_URL: 'http://nginx/api'
+module.exports = {
+    webpackDevMiddleware: config => {
+        config.watchOptions = {
+            poll: 1000,
+            aggregateTimeout: 300,
         }
+
+        return config
+    },
+    generateBuildId: async () => {
+        return 'frontend'
+    },
+    env: {
+        NEXT_PUBLIC_HOST_URL: process.env.NEXT_PUBLIC_HOST_URL,
+        NEXT_PUBLIC_HOST_API_URL: process.env.NEXT_PUBLIC_HOST_API_URL,
+        HOST_SSR_URL: process.env.HOST_SSR_URL,
+        HOST_SSR_API_URL: process.env.HOST_SSR_API_URL
     }
 }
